690d80dec25017701724d3fddb5fcc70d1f570da
Ivan Ershov <ivershov9@gmail.com>
2019-07-03 20:07:21 +0300

    truncate, stripHtml

---
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/String.kt
@@ -0,0 +1,15 @@
+package ru.skillbranch.devintensive.extensions
+
+fun String.truncate(len: Int = 16): String {
+    val result = trim()
+    if (result.length <= len)
+        return result
+    return result.take(len).trimEnd() + "..."
+}
+
+fun String.stripHtml(): String {
+    val regex1 = """&.*?;|<.*?>""".toRegex()
+    val regex2 = """[ ]{2,}""".toRegex()
+
+    return replace(regex1, "").replace(regex2, " ")
+}
