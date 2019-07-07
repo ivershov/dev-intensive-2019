ca0c8dc6ee5cdf95364610927274dd5651361ef9
Ivan Ershov <ivershov9@gmail.com>
2019-07-07 14:14:34 +0300

    fix truncate

--- app/src/main/java/ru/skillbranch/devintensive/extensions/String.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/String.kt
@@ -1,6 +1,6 @@
 package ru.skillbranch.devintensive.extensions
 
-fun String.truncate(len: Int = 17): String {
+fun String.truncate(len: Int = 16): String {
     val result = trim()
     if (result.length <= len)
         return result
@@ -9,7 +9,7 @@ fun String.truncate(len: Int = 17): String {
 
 fun String.stripHtml(): String {
     val regex1 = """&.*?;|<.*?>""".toRegex()
-    val regex2 = """[ ]{2,}""".toRegex()
+    val regex2 = """[ ]+""".toRegex()
 
     return replace(regex1, "").replace(regex2, " ")
 }
